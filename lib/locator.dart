b52b76116028f88307dba0b7008c3c9ee46ae426
Jason-Omondi <jason.naboth@gmail.com>
2025-10-26 17:58:54 +0300

    fix: db instance locator

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,9 +1,14 @@
 import 'package:get_it/get_it.dart';
 
+import 'database/app_db.dart';
 import 'repositories/vocabulary_repository.dart';
 
 GetIt locator = GetIt.instance;
 
 void setUp() {
+  // Register the database first
+  locator.registerLazySingleton(() => AppDb());
+
+  // Then register the repository
   locator.registerLazySingleton(() => VocabularyRepository());
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:tubongeapp/locator.dart';
 
 import 'provider/vocabulary_provider.dart';
+import 'screens/add_vocab_screen.dart';
+import 'screens/vocab_home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,52 +24,11 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: const MyHomePage(title: 'Dashboard'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        home: VocabHome(),
+        routes: {
+          '/vocab-home': (context) => VocabHome(),
+          '/add-vocabulary': (context) => AddVocabScreen(),
+        },
       ),
     );
   }
